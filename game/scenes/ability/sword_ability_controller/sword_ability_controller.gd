be9aab023d0199adeb726a26258843c0642601e7
Sean Carlyle <scarlyle20@gmail.com>
2023-10-31 13:52:10 -0700

    adding sword animations

--- game/scenes/ability/sword_ability_controller/sword_ability_controller.gd
+++ game/scenes/ability/sword_ability_controller/sword_ability_controller.gd
@@ -1,12 +1,39 @@
 extends Node
 
+const MAX_RANGE = 150
+
 @export var sword_ability: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# get_node("Timer")
+	$Timer.timeout.connect(on_timer_timeout)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func on_timer_timeout():
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	if player == null:
+		return
+	
+	var enemies = get_tree().get_nodes_in_group("enemy")
+	enemies = enemies.filter(func(enemy: Node2D): 
+		return enemy.global_position.distance_squared_to(player.global_position) < pow(MAX_RANGE, 2)
+	)
+	
+	if enemies.size() == 0:
+		return
+	
+	# Sort the enemies by who is closer to player
+	enemies.sort_custom(func(a: Node2D, b: Node2D): 
+		var a_distance = a.global_position.distance_squared_to(player.global_position)
+		var b_distance = b.global_position.distance_squared_to(player.global_position)
+		return a_distance < b_distance
+	)
+	
+	var sword_instance = sword_ability.instantiate() as Node2D
+	player.get_parent().add_child(sword_instance)
+	sword_instance.global_position = enemies[0].global_position
